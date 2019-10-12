d36fb8937d36fb43e925a2ba800ca58f27c6f153
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-10-12 16:19:37 +0100

    Add Out operation (not yet finished)
    this is the only operation that really does anything, and it works kind
    of unusally because you cannot query by grafana annotation ID
    
    When we put to a resource, we should check if the configured directory
    contains a file called "id"
    
    If there is an "id" file it means we have already created an annotation
    and so we should update it with the current timestamp
    
    If there is no "id" file then we should create a new annotation, create
    a file called "id" in the targeted directory with the new annotation ID
    as contents.
    
    When we create a new annotation, always give it a timeend for now
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ pkg/out/out.go
@@ -0,0 +1,224 @@
+package out
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"time"
+
+	"github.com/alphagov/paas-grafana-annotation-resource/pkg/tags"
+	"github.com/alphagov/paas-grafana-annotation-resource/pkg/types"
+)
+
+const defaultTemplate = "${BUILD_ID} ${ATC_EXTERNAL_URL}/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NAME}/jobs/${BUILD_JOB_NAME}/builds/${BUILD_NAME}"
+
+func pathToIDFile(dirPath string) string {
+	return path.Join(dirPath, "id")
+}
+
+func Out(
+	req types.OutRequest,
+	env map[string]string,
+	dirPath string) (types.InOutResponse, error) {
+	_, err := os.Stat(pathToIDFile(dirPath))
+
+	if err == nil {
+		// put to resource, id file exists, updating existing annotation
+		return outUpdate(req, env, dirPath)
+	}
+
+	if os.IsNotExist(err) {
+		// put to resource, id file does not exist, creating new annotation
+		return outCreate(req, env, dirPath)
+	}
+
+	// unknown error
+	return types.InOutResponse{}, err
+}
+
+func addGrafanaAPIHeaders(req *http.Request) {
+	req.Header.Set("Content-Type", "application/json")
+}
+
+func addGrafanaAuth(req *http.Request, username string, password string) {
+	req.SetBasicAuth(username, password)
+}
+
+func outCreate(
+	req types.OutRequest,
+	env map[string]string,
+	dirPath string,
+) (types.InOutResponse, error) {
+
+	combinedTags := tags.CombineTags(req.Source.Tags, req.Params.Tags)
+
+	actualTemplate := defaultTemplate
+	if req.Source.Template != nil {
+		actualTemplate = *req.Source.Template
+	}
+	if req.Params.Template != nil {
+		actualTemplate = *req.Params.Template
+	}
+
+	text := os.Expand(actualTemplate, func(varName string) string {
+		if val, ok := req.Params.Env[varName]; ok {
+			return val
+		}
+
+		if val, ok := req.Source.Env[varName]; ok {
+			return val
+		}
+
+		if val, ok := env[varName]; ok {
+			return val
+		}
+
+		return "nil"
+	})
+
+	currentTime := time.Now().Unix() * int64(1000)
+
+	requestBody := types.GrafanaCreateAnnotationRequest{
+		Time:    currentTime,
+		TimeEnd: currentTime + 1,
+		Tags:    combinedTags,
+		Text:    text,
+	}
+
+	requestBodyBytes, err := json.Marshal(requestBody)
+
+	if err != nil {
+		return types.InOutResponse{}, err
+	}
+
+	url := fmt.Sprintf("%s/api/annotations", req.Source.URL)
+
+	httpReq, err := http.NewRequest(
+		"POST", url, bytes.NewReader(requestBodyBytes),
+	)
+
+	if err != nil {
+		return types.InOutResponse{}, err
+	}
+
+	addGrafanaAPIHeaders(httpReq)
+	addGrafanaAuth(httpReq, req.Source.Username, req.Source.Password)
+
+	httpResp, err := http.DefaultClient.Do(httpReq)
+
+	if err != nil {
+		return types.InOutResponse{}, err
+	}
+
+	respBodyBytes, err := ioutil.ReadAll(httpResp.Body)
+
+	if err != nil {
+		return types.InOutResponse{}, err
+	}
+
+	if httpResp.StatusCode != 200 {
+		return types.InOutResponse{}, fmt.Errorf(
+			fmt.Sprintf(
+				"Expected 200 from Grafana API, %d received: %s",
+				httpResp.StatusCode, string(respBodyBytes),
+			),
+		)
+	}
+
+	var parsedResponse types.GrafanaCreateAnnotationResponse
+	err = json.Unmarshal(respBodyBytes, &parsedResponse)
+
+	if err != nil {
+		return types.InOutResponse{}, err
+	}
+
+	annotationID := fmt.Sprintf("%d", parsedResponse.ID)
+
+	err = ioutil.WriteFile(pathToIDFile(dirPath), []byte(annotationID), 0644)
+
+	if err != nil {
+		return types.InOutResponse{}, err
+	}
+
+	return types.InOutResponse{
+		Version: types.ResourceVersion{
+			ID: annotationID,
+		},
+		Metadata: []types.ResourceMetadataPair{
+			{Name: "id", Value: annotationID},
+			{Name: "tags", Value: tags.FormatTags(combinedTags)},
+			{Name: "text", Value: text},
+		},
+	}, nil
+}
+
+func outUpdate(
+	req types.OutRequest,
+	env map[string]string,
+	dirPath string,
+) (types.InOutResponse, error) {
+
+	idBytes, err := ioutil.ReadFile(pathToIDFile(dirPath))
+
+	if err != nil {
+		return types.InOutResponse{}, err
+	}
+
+	requestBody := types.GrafanaCreateAnnotationRequest{
+		TimeEnd: time.Now().Unix() * int64(1000),
+	}
+
+	requestBodyBytes, err := json.Marshal(requestBody)
+
+	if err != nil {
+		return types.InOutResponse{}, err
+	}
+
+	annotationID := string(idBytes)
+	url := fmt.Sprintf("%s/api/annotations/%s", req.Source.URL, annotationID)
+
+	httpReq, err := http.NewRequest(
+		"PATCH", url, bytes.NewReader(requestBodyBytes),
+	)
+
+	if err != nil {
+		return types.InOutResponse{}, err
+	}
+
+	addGrafanaAPIHeaders(httpReq)
+	addGrafanaAuth(httpReq, req.Source.Username, req.Source.Password)
+
+	httpResp, err := http.DefaultClient.Do(httpReq)
+
+	if err != nil {
+		return types.InOutResponse{}, err
+	}
+
+	respBodyBytes, err := ioutil.ReadAll(httpResp.Body)
+
+	if err != nil {
+		return types.InOutResponse{}, err
+	}
+
+	if httpResp.StatusCode != 200 {
+		return types.InOutResponse{}, fmt.Errorf(
+			fmt.Sprintf(
+				"Expected 200 from Grafana API, %d received: %s",
+				httpResp.StatusCode, string(respBodyBytes),
+			),
+		)
+	}
+
+	return types.InOutResponse{
+		Version: types.ResourceVersion{
+			ID: annotationID,
+		},
+		Metadata: []types.ResourceMetadataPair{
+			{Name: "id", Value: annotationID},
+		},
+	}, nil
+}
---
+++ pkg/out/out_test.go
@@ -0,0 +1,302 @@
+package out_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"testing"
+	"time"
+
+	"github.com/jarcoal/httpmock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/alphagov/paas-grafana-annotation-resource/pkg/out"
+	"github.com/alphagov/paas-grafana-annotation-resource/pkg/types"
+)
+
+func TestCheck(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Out Suite")
+}
+
+var _ = Describe("Out", func() {
+	const (
+		username = "admin"
+		password = "password"
+	)
+
+	var (
+		resourceDirectory *string
+
+		basicAuthCheckResponderWrapper = func(
+			wrapped httpmock.Responder,
+		) httpmock.Responder {
+			return func(req *http.Request) (*http.Response, error) {
+				rUsername, rPassword, rAuthEnabled := req.BasicAuth()
+
+				Expect(rAuthEnabled).To(
+					Equal(true), "Basic auth should be enabled",
+				)
+
+				Expect(rUsername).To(
+					Equal(username),
+					fmt.Sprintf(
+						"Basic auth username should be '%s' was '%s'",
+						username, rUsername,
+					),
+				)
+
+				Expect(rPassword).To(
+					Equal(password),
+					fmt.Sprintf(
+						"Basic auth password should be '%s' was '%s'",
+						password, rPassword,
+					),
+				)
+
+				return wrapped(req)
+			}
+		}
+
+		env map[string]string = map[string]string{
+			"BUILD_ID":            "build-id",
+			"BUILD_NAME":          "build-name",
+			"BUILD_JOB_NAME":      "build-job-name",
+			"BUILD_PIPELINE_NAME": "build-pipeline-name",
+			"BUILD_TEAM_NAME":     "build-team-name",
+			"ATC_EXTERNAL_URL":    "http://concourse-url",
+		}
+	)
+
+	BeforeSuite(func() {
+		httpmock.Activate()
+	})
+
+	AfterSuite(func() {
+		httpmock.DeactivateAndReset()
+	})
+
+	BeforeEach(func() {
+		httpmock.Reset()
+
+		dir, err := ioutil.TempDir("", "")
+		Expect(err).NotTo(HaveOccurred())
+
+		resourceDirectory = &dir
+	})
+
+	AfterEach(func() {
+		if resourceDirectory != nil {
+			os.RemoveAll(*resourceDirectory)
+		}
+
+		resourceDirectory = nil
+	})
+
+	Context("when validating the source", func() {
+	})
+
+	Context("when validating the params", func() {
+	})
+
+	Context("when no id file exists", func() {
+		var (
+			req types.OutRequest
+		)
+
+		BeforeEach(func() {
+			httpmock.RegisterResponder(
+				"POST", "http://grafana/api/annotations",
+				basicAuthCheckResponderWrapper(
+					func(req *http.Request) (*http.Response, error) {
+						bodyBytes, err := ioutil.ReadAll(req.Body)
+						Expect(err).NotTo(HaveOccurred())
+
+						var requestBody types.GrafanaCreateAnnotationRequest
+						err = json.Unmarshal(bodyBytes, &requestBody)
+						Expect(err).NotTo(HaveOccurred())
+
+						Expect(requestBody.Text).To(
+							Equal("build-id env-var-source env-var-param"),
+							"Text interpolation should work",
+						)
+
+						Expect(requestBody.Tags).To(
+							ConsistOf("p1", "p2", "s1", "s2"),
+							"Tags should be present and correct",
+						)
+
+						Expect(requestBody.Time).To(BeNumerically(
+							"==", time.Now().Unix()*int64(1000), 1500,
+						), "Time should approximately be now")
+
+						Expect(requestBody.TimeEnd).To(BeNumerically(
+							"==", time.Now().Unix()*int64(1000), 1500,
+						), "TimeEnd should approximately be now")
+
+						responder := httpmock.NewStringResponder(
+							200,
+							`{ "message":"Annotation added", "id": 12345 }`,
+						)
+
+						return responder(req)
+					},
+				),
+			)
+
+			sourceTemplate := "this will be overridden by the param template"
+			paramsTemplate := "${BUILD_ID} ${ENV_VAR_SOURCE} ${ENV_VAR_PARAM}"
+
+			req = types.OutRequest{
+				Source: types.ResourceSource{
+					URL: "http://grafana",
+
+					Username: username,
+					Password: password,
+
+					Template: &sourceTemplate,
+
+					Tags: []string{"s2", "s1"},
+					Env: map[string]string{
+						"ENV_VAR_SOURCE": "env-var-source",
+					},
+				},
+
+				Params: types.ResourceParams{
+					Template: &paramsTemplate,
+
+					Tags: []string{"p2", "p1"},
+					Env: map[string]string{
+						"ENV_VAR_PARAM": "env-var-param",
+					},
+				},
+			}
+		})
+
+		AfterEach(func() {
+			callCount := httpmock.GetTotalCallCount()
+			Expect(callCount).To(Equal(1), "Out should make an API call to Grafana")
+		})
+
+		It("should return the created id within the version", func() {
+			resp, err := out.Out(req, env, *resourceDirectory)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(resp.Version.ID).To(Equal("12345"))
+		})
+
+		It("should return metadata", func() {
+			resp, err := out.Out(req, env, *resourceDirectory)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(resp.Metadata).To(HaveLen(3))
+
+			Expect(resp.Metadata).To(ContainElement(
+				types.ResourceMetadataPair{Name: "id", Value: "12345"},
+			))
+			Expect(resp.Metadata).To(ContainElement(
+				types.ResourceMetadataPair{Name: "tags", Value: "p1, p2, s1, s2"},
+			))
+			Expect(resp.Metadata).To(ContainElement(
+				types.ResourceMetadataPair{
+					Name:  "text",
+					Value: "build-id env-var-source env-var-param",
+				},
+			))
+		})
+
+		It("should create an 'id' file containing the version", func() {
+			_, err := out.Out(req, env, *resourceDirectory)
+			Expect(err).NotTo(HaveOccurred())
+
+			idPath := path.Join(*resourceDirectory, "id")
+
+			_, err = os.Stat(idPath)
+			Expect(err).NotTo(HaveOccurred(), "Out should create a file 'id'")
+
+			idBytes, err := ioutil.ReadFile(idPath)
+			Expect(err).NotTo(HaveOccurred(), "Out should create readable file 'id'")
+
+			Expect(string(idBytes)).To(
+				Equal("12345"), "Out should write the id to 'id'",
+			)
+		})
+	})
+
+	Context("when an id file exists", func() {
+		const (
+			annotationID = "12345"
+		)
+
+		var (
+			req types.OutRequest
+		)
+
+		BeforeEach(func() {
+			Expect(ioutil.WriteFile(
+				path.Join(*resourceDirectory, "id"), []byte(annotationID), 0644,
+			)).NotTo(HaveOccurred(), "Could not write id file needed for test")
+
+			httpmock.RegisterResponder(
+				"PATCH", "http://grafana/api/annotations/"+annotationID,
+				basicAuthCheckResponderWrapper(
+					func(req *http.Request) (*http.Response, error) {
+						bodyBytes, err := ioutil.ReadAll(req.Body)
+						Expect(err).NotTo(HaveOccurred())
+
+						var requestBody types.GrafanaUpdateAnnotationRequest
+						err = json.Unmarshal(bodyBytes, &requestBody)
+						Expect(err).NotTo(HaveOccurred())
+
+						Expect(requestBody.TimeEnd).To(BeNumerically(
+							"==", time.Now().Unix()*int64(1000), 1500,
+						), "TimeEnd should approximately be now")
+
+						responder := httpmock.NewStringResponder(
+							200,
+							`{ "message":"Annotation patched"}`,
+						)
+
+						return responder(req)
+					},
+				),
+			)
+
+			req = types.OutRequest{
+				Source: types.ResourceSource{
+					URL: "http://grafana",
+
+					Username: username,
+					Password: password,
+				},
+			}
+		})
+
+		AfterEach(func() {
+			callCount := httpmock.GetTotalCallCount()
+			Expect(callCount).To(Equal(1), "Out should make an API call to Grafana")
+		})
+
+		It("should return the created id within the version", func() {
+			resp, err := out.Out(req, env, *resourceDirectory)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(resp.Version.ID).To(Equal("12345"))
+		})
+
+		It("should return limited metadata", func() {
+			resp, err := out.Out(req, env, *resourceDirectory)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(resp.Metadata).To(HaveLen(1))
+
+			Expect(resp.Metadata).To(ContainElement(
+				types.ResourceMetadataPair{Name: "id", Value: "12345"},
+			))
+		})
+	})
+})
