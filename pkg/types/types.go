4b822b704e17c13d33f791214a7e539608231b44
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-10-12 16:17:35 +0100

    Add types pkg
    These will be used in other packages
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ pkg/types/types.go
@@ -0,0 +1,67 @@
+package types
+
+type ResourceVersion struct {
+	ID string `json:"id"`
+}
+
+type ResourceSource struct {
+	URL string `json:"uri"`
+
+	Username string `json:"username"`
+	Password string `json:"password"`
+
+	Template *string `json:"template"`
+
+	Tags []string          `json:"tags"`
+	Env  map[string]string `json:"env"`
+}
+
+type ResourceParams struct {
+	Template *string `json:"template"`
+
+	Tags []string          `json:"tags"`
+	Env  map[string]string `json:"env"`
+}
+
+type ResourceMetadataPair struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+type CheckRequest struct {
+	Source  ResourceSource   `json:"source"`
+	Version *ResourceVersion `json:"version"`
+}
+
+type CheckResponse = []ResourceVersion
+
+type InRequest struct {
+	Source  ResourceSource  `json:"source"`
+	Version ResourceVersion `json:"version"`
+}
+
+type OutRequest struct {
+	Source ResourceSource `json:"source"`
+	Params ResourceParams `json:"params"`
+}
+
+type InOutResponse struct {
+	Version  ResourceVersion `json:"version"`
+	Metadata []ResourceMetadataPair
+}
+
+type GrafanaCreateAnnotationRequest struct {
+	Time    int64    `json:"time"`
+	TimeEnd int64    `json:"timeEnd"`
+	Tags    []string `json:"tags"`
+	Text    string   `json:"text"`
+}
+
+type GrafanaUpdateAnnotationRequest struct {
+	TimeEnd int64 `json:"timeEnd"`
+}
+
+type GrafanaCreateAnnotationResponse struct {
+	Message string `json:"message"`
+	ID      int    `json:"id"`
+}
